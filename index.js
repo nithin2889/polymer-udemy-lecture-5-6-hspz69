678b5e409151e137eb17d11b7dfad4ad9fd652ce
nithin2889 <nithin2889@gmail.com>
2020-08-20 13:35:44 +0530

    Shadow DOM, slots, and styles

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
  import {LitElement, html} from '@polymer/lit-element';
+ import "@polymer/paper-checkbox/paper-checkbox.js"
 
 class MyElement extends LitElement {
 
-  _render() {
-    return html`<h1>Polymer is GREAT<h1>`
+  static get properties() { return { mood: String }}
+
+  _render({ mood }) {
+    return html`
+      <style>
+        div {background-color: grey}
+      </style>
+      <div class="test">
+        Web components are <span class="mood">${mood}</span>!<br>
+        <paper-checkbox>Ready to deploy</paper-checkbox>
+      </div>`;
   }
 }
 
